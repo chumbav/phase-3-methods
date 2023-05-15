06aae4fbb08c9e582c2bc5e90a2e66f591e3ece1
Victor <victor.chumba@student.moringaschool.com>
2023-05-15 16:00:21 +0300

    completed lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end 
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+ greet ('Naureen')
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default "Sunny"
+  
+def add(num1,num2)
+    return num1 + num2
+end
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    else
+        return number/2
+    end
+end
+halve(10)
